5a923b02be949f51fc7aa17c03e01a0a32bd4639
qkrcodus <qkrcodus0722@gmail.com>
2025-10-18 09:11:13 +0900

    docs(readme) : update readme

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String rawInput = Console.readLine();
-        System.out.println("결과 : 0");
+        System.out.println("결과: 0");
     }
 }
