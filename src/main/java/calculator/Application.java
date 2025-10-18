3370cbd55d5396425de4a60af5be2af2fae2b812
qkrcodus <qkrcodus0722@gmail.com>
2025-10-18 10:23:37 +0900

    feat(parse) : find custom delimiter and select parseTarget

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,12 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String rawInput= Console.readLine();
-        int result= StringCalculator.sum(Parser.toInts(rawInput));
-        System.out.println("결과: "+ result);
+        String rawInput = Console.readLine();
+        int[] sumTarget = Parser.parseAndConvertToInts(rawInput);
+        int result = StringCalculator.sum(sumTarget);
+        System.out.println("결과: " + result);
     }
 }
--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -5,12 +5,27 @@ public final class Parser {
     }
 
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String PREFIX_DELIMITER = "//";
+    private static final char SUFFIX_DELIMITER = '\n';
 
-    public static int[] toInts(String raw) {
+    public static int[] parseAndConvertToInts(String raw) {
         String norm = (raw == null) ? "" : raw.replace("\\n", "\n");
         if (norm.isBlank()) {
             return new int[0];
         }
-        return new int[]{0};
+
+        String delimiter = DEFAULT_DELIMITER;
+        String parseTarget = norm;
+
+        if (norm.startsWith(PREFIX_DELIMITER)) {
+            int idx = norm.indexOf(SUFFIX_DELIMITER);
+            if (idx > 0) {
+                String customDelimiter = norm.substring(PREFIX_DELIMITER.length(), idx);
+                delimiter += customDelimiter;
+                parseTarget = norm.substring(idx + 1);
+            }
+        }
+
+        return new int[0];
     }
 }
